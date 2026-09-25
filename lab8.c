2fc051ae003d7f5db06d2354106b19b81ee45de9
Sohan S Salian <sohansalian124@gmail.com>
2026-09-25 14:16:07 +0530

    Improve formatting and readability of lab8.c
    Refactor MPI program for clarity and formatting.

--- lab8.c
+++ lab8.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char** argv){
-    int rank,size,data[4]={10,20,30,40},recv;
+int main(int argc, char** argv) {
+    int rank, size, data[4] = {10, 20, 30, 40}, recv;
 
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    MPI_Init(&argc, &argv);
 
-    MPI_Scatter(data,1,MPI_INT,&recv,1,MPI_INT,0,MPI_COMM_WORLD);
-    recv+=1;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if(rank==0){
+    MPI_Scatter(data, 1, MPI_INT, &recv, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    printf("Process %d received: %d\n", rank, recv);
+
+    recv += 1;
+
+    MPI_Gather(&recv, 1, MPI_INT, data, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
         printf("Gathered data: ");
-        for(int i=0;i<size;i++) printf("%d",data[i]);
+        for (int i = 0; i < size; i++)
+            printf("%d ", data[i]);
         printf("\n");
     }
 
     MPI_Finalize();
     return 0;
 }
-# output:Gathered data: 10203040
